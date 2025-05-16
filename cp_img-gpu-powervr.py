033a9424a4c6497311829e0c99f72ad07cb97335
liangbaihui <baihui.liang@spacemit.com>
2025-05-16 15:05:28 +0800

    Fix: To avoid conflicts with Mesa, install libpvr_mesa_wsi.so to /usr/local/lib.

--- cp_img-gpu-powervr.py
+++ cp_img-gpu-powervr.py
@@ -48,8 +48,13 @@ def copy_usr(source_dir, destination_dir):
 	for file in cur_files:
 		shutil.copy(os.path.join(source_dir, cure_path, file), os.path.join(destination_dir, file))
 
+    print(f"Copied {cur_files} to {destination_dir}")
 
-	print(f"Copied {cur_files} to {destination_dir}")
+    destination_dir = "./target/usr/local/lib/"
+    file = libpvr_mesa_wsi.so
+    shutil.copy(os.path.join(source_dir, cure_path, file), os.path.join(destination_dir, file))
+
+    print(f"Copied {file} to {destination_dir}")
 
 
 def copy_txtFiles(source_dir, destination_dir):
